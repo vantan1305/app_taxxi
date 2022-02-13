b475e1b188b62dba8cdc4ff8f3bb292c5ec77f42
Tan Tan <phamvantan135@gmail.com>
2022-02-13 22:35:25 +0700

    commit 1

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:register_login/register_login/loginApp.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginApp());
+  }
+}
